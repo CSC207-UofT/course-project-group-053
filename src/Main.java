4d2533911eafd8a841b15c8ef5a6c9c673b32466
aliza <alizaazizlakho>
2021-10-11 19:34:54 -0400

    Player added in Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
+
 public class Main {
+    public static String Black = "B";
+    public static String White = "W";
 
     public static void main(String[] args){
         Start();
@@ -14,26 +17,24 @@ public class Main {
         String pla1 = sc.nextLine();
         System.out.print("Choose Colour for: " + pla1 + ". Type B for Black or W for white \r\n");
         String col1 = sc.nextLine();
-        while (!col1.equals("B") & !col1.equals("W")){
+        while (!col1.equals(Black) & !col1.equals(White)){
             System.out.print("Choose Colour for: " + pla1 + ". Type B for Black or W for white \r\n");
             col1 = sc.nextLine();
         }
-        // Player player1 = null;
-        // p1 = new HumanPlayer(name = pla1, colour = col1);
+        HumanPlayer1 player1 = new HumanPlayer1(pla1, col1);
         String color;
         String col2;
-        if (col1.equals("W")){
+        if (col1.equals(White)){
             color = "Black";
-            col2 = "B";
+            col2 = Black;
         }else{
             color = "White";
-            col2 = "W";
+            col2 = White;
         }
         System.out.print("Type name of the Human Player 2: \r\n");
         String pla2 = sc.nextLine();
         System.out.print("The colour for " + pla2 + " is " + color + "\r\n");
-        // Player player2 = null;
-        // p2 = new HumanPlayer(name = pla2, colour = col2);
+        HumanPlayer1 player2 = new HumanPlayer1(pla2, col2);
     }
 
     public static void IniGameBoard(){
