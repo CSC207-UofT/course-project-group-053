a95d31b21910a90d406f756671cc1e8cc18632eb
marynam99 <mary.nam@mail.utoronto.ca>
2021-10-12 11:46:28 -0400

    Player class methods modified to public

--- src/Player.java
+++ src/Player.java
@@ -1,42 +1,98 @@
 public abstract class Player {
     String str_player_username;
+    String str_player_tokencolour;
 
     //Keep track of player’s chips
     Integer int_player_numchipsleft;
     Integer int_player_numchipsonboard;
 
-    public Player(String str_player_username) {
+    public Player(String str_player_username, String colour) {
         this.str_player_username = str_player_username;
+        this.str_player_tokencolour = colour;
 
         //initialize with 9 chips left and 0 on board
         this.int_player_numchipsleft = 9;
         this.int_player_numchipsonboard = 0;
     }
 
-    private void reset() {
+    public void reset() {
         this.int_player_numchipsleft = 9;
         this.int_player_numchipsonboard = 9;
     }
 
-    private String get_username() {
+    public String get_username() {
         return this.str_player_username;
     }
 
-    private int get_numchipsleft() {
+    public String get_tokencolour() { return this.str_player_tokencolour; }
+
+    public int get_numchipsleft() {
         return this.int_player_numchipsleft;
     }
 
-    private int get_numchipsonboard() {
+    public int get_numchipsonboard() {
         return this.int_player_numchipsonboard;
     }
 
     //decrease or increase number of chips
-    private void dec_numchipsleft() {
+    public void dec_numchipsleft() {
         //used when other player gets house. with dec_numchipsonboard()
         --this.int_player_numchipsleft;
     }
 
-    private void inc_numchipsonboard() {
+    public void inc_numchipsonboard() {
+        ++this.int_player_numchipsonboard;
+    }
+
+    private void dec_numchipsonboard() {
+        --this.int_player_numchipsonboard;
+    }
+
+    public abstract boolean isHuman();
+}
+public abstract class Player {
+    String str_player_username;
+    String str_player_tokencolour;
+
+    //Keep track of player’s chips
+    Integer int_player_numchipsleft;
+    Integer int_player_numchipsonboard;
+
+    public Player(String str_player_username, String colour) {
+        this.str_player_username = str_player_username;
+        this.str_player_tokencolour = colour;
+
+        //initialize with 9 chips left and 0 on board
+        this.int_player_numchipsleft = 9;
+        this.int_player_numchipsonboard = 0;
+    }
+
+    public void reset() {
+        this.int_player_numchipsleft = 9;
+        this.int_player_numchipsonboard = 9;
+    }
+
+    public String get_username() {
+        return this.str_player_username;
+    }
+
+    public String get_tokencolour() { return this.str_player_tokencolour; }
+
+    public int get_numchipsleft() {
+        return this.int_player_numchipsleft;
+    }
+
+    public int get_numchipsonboard() {
+        return this.int_player_numchipsonboard;
+    }
+
+    //decrease or increase number of chips
+    public void dec_numchipsleft() {
+        //used when other player gets house. with dec_numchipsonboard()
+        --this.int_player_numchipsleft;
+    }
+
+    public void inc_numchipsonboard() {
         ++this.int_player_numchipsonboard;
     }
 
