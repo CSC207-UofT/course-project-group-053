b7029c268b80e6c12f147d10d445ca8993bf02bd
marynam99 <mary.nam@mail.utoronto.ca>
2021-10-15 09:32:08 -0400

    Add GBM Test

--- test/GameBoardManagerTest.java
+++ test/GameBoardManagerTest.java
@@ -1,8 +1,57 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-// TODO - add tests later
+class GameBoardManagerTest {
+    GameBoardManager gameBoardManager;
+    GameBoard gameBoard;
 
+    @BeforeEach
+    void setUp() {
+        gameBoardManager = new GameBoardManager();
+        gameBoard = new GameBoard();
+    }
 
-public class GameBoardManagerTest {
+    @AfterEach
+    void tearDown() {
+        gameBoardManager = null;
+    }
 
+    @Test
+    void getGameBoardState() {
+    }
+
+    @Test
+    void processPlayerMove() throws InvalidPositionException, OccupiedSlotException {
+        gameBoardManager.processPlayerMove("W", "A1");
+
+        //when placing on occupied spot
+        gameBoardManager.processPlayerMove("B","A1");
+    }
+
+    @Test
+    void getPlayer1Houses() {
+    }
+
+    @Test
+    void getPlayer2Houses() {
+    }
+
+    @Test
+    void checkPhaseOneEnd() {
+    }
+
+    @Test
+    void millAdder() {
+    }
+
+    @Test
+    void checkHouse() {
+    }
+
+    @Test
+    void processPlayerRemove() {
+    }
 }
---
+++ test/MainTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void gamePlay() {
+    }
+}
