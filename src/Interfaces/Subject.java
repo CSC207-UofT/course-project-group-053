94609af5878fa2b18396973cfe6d333f93fe8649
Jason-B-Jiang <jasonbitan.jiang@mail.utoronto.ca>
2021-12-05 22:25:38 -0500

    finished javadocs for Subject interface

--- src/Interfaces/Subject.java
+++ src/Interfaces/Subject.java
@@ -2,7 +2,22 @@ package Interfaces;
 import Entity.Token;
 
 public interface Subject {
+    /**
+     * Add an Observer for this Subject to update
+     * @param o Observer object to be updated
+     */
     void register(Observer o);
+
+    /**
+     * Update Observers about token being added to gameboard
+     * @param position Gameboard position where token was added
+     * @param playerToken Token that was added
+     */
     void notifyObservers(String position, Token playerToken);
+
+    /**
+     * Update Observers about token being removed from gameboard
+     * @param position Gameboard position from which token was removed
+     */
     void notifyObservers(String position);
 }
